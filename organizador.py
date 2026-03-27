2ee8f2c684b568acace0070ab60111b766a44371
youfernandodark <darkskull.adriask8@hotmail.com>
2026-03-26 22:08:37 -0300

    Add files via upload

---
+++ organizador.py
@@ -0,0 +1,38 @@
+meu_vod = [
+    {
+        "id": "TerrorEstrada2024",
+        "titulo": "Terror na Estrada",
+        "grupo": "Filmes", 
+        "logo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwFH4s4vcllU_up3cxXKTFeZjcjGZiQDxgjlf1igAIWw&s=10",
+        "url": "https://video.wixstatic.com/video/db47f8_4671182ae2b347e888c6b5214bf9286d/720p/mp4/file.mp4",
+        "ano": "2024",
+        [span_2](start_span)"sinopse": "Uma jornada aterrorizante por estradas desertas."[span_2](end_span)
+    },
+    {
+        "id": "Gladiador2_2024",
+        "titulo": "Gladiador 2",
+        "grupo": "Filmes", 
+        "logo": "https://link_da_imagem_aqui.jpg",
+        "url": "URL_DO_VIDEO_AQUI",
+        "ano": "2024",
+        "sinopse": "Anos após a morte de Maximus, Lucius entra no Coliseu para enfrentar o Império."
+    },
+    {
+        "id": "Batman_2022",
+        "titulo": "The Batman",
+        "grupo": "Filmes", 
+        "logo": "https://link_da_imagem_aqui.jpg",
+        "url": "URL_DO_VIDEO_AQUI",
+        "ano": "2022",
+        "sinopse": "O vigilante de Gotham investiga uma série de assassinatos enigmáticos."
+    },
+    {
+        "id": "LCDP_S01E01",
+        "titulo": "La Casa de Papel S01 E01 - Efetuado",
+        "grupo": "La Casa de Papel", 
+        "logo": "https://m.media-amazon.com/images/M/MV5BN2Q2NmZjMzQtM2ZhYS00NWM3LTg0MDktYjNkNWYzM2MyYjI5XkEyXkFqcGc@._V1_.jpg",
+        "url": "https://video.wixstatic.com/video/8d3786_251e5a43d9384b21ab6525a558d1d830/720p/mp4/file.mp4",
+        "ano": "2017",
+        [span_3](start_span)"sinopse": "O Professor recruta uma jovem assaltante e outros sete criminosos."[span_3](end_span)
+    }
+]
